6e7d4c28448c6d52fbe9b4f4df0a45e04a0a25a4
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-06-26 17:43:23 +0200

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,1 +1,2 @@
-git clone https://github.com/vikdevelop/fedora-postconfig /tmp/fedora-postconfig && cd /tmp/fedora-postconfig && python3 fedora-postconfig && cd && rm -rf /tmp/fedora-postconfig
+git clone https://github.com/vikdevelop/fedora-postinstall-config /tmp/fedora-postinstall-config && cd /tmp/fedora-postinstall-config && python3 fedora-postconfig && cd && rm -rf /tmp/fedora-postinstall-config
+
