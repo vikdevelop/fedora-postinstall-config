62248b640257efb9113d50865dac520673f39f6c
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-06-27 09:37:31 +0200

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/vikdevelop/fedora-postinstall-config /tmp/fedora-postinstall-config 
 cd /tmp/fedora-postinstall-config
-pkexec python3 fedora-postinstall-config 
+echo "Tento skript vyžaduje prává uživatele superuser. Pro pokračování zadejte heslo:"
+sudo python3 fedora-postinstall-config 
 cd 
 rm -rf /tmp/fedora-postinstall-config
