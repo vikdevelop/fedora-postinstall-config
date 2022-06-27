fbcd99b5b06a8541feabf4ba5acfa08369428834
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-06-27 09:15:54 +0200

    Update gpu.py

--- scripts/gpu.py
+++ scripts/gpu.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 import os
 if os.path.exists("/dev/nvidia0"):
-    print("-")
+    print("→ installation of proprietary driver by nVidia")
+    nvidia = input("→→ do you wish install proprietary driver by nVidia? [Y/n]: ")
+    # NVIDIA proprietary graphic card driver installation
+    if nvidia == 'n':
+        print("I skiping.")
+    elif nvidia == 'y' or 'Y':
+        print("→ before installation proprietary driver, we first check, if updates avaible again:")
+        os.system("sudo dnf update --refresh -y")
+        print("→ instalování prop. ovladače nVidia Linux akmod graphic card driver:")
+        os.system("sudo dnf install -y akmod-nvidia")
+        print('\033[1m' + '→→ OK' + '\033[0m')
 else:
     print("→ installation of program CoreCtrl for simple settings GPUs by AMD")
     corectrl = input("→→ so do you wish install corectrl? [Y/n]: ")
