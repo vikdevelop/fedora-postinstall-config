5aec513dc3a056f45966e571137db4ce355ce3e4
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-06-29 18:22:58 +0200

    Update gpu.py

--- scripts/gpu.py
+++ scripts/gpu.py
@@ -6,7 +6,6 @@ from en import *
 if os.path.exists("/dev/nvidia0"):
     print(nvidiagpu_installation_title)
     nvidia = input(nvidiagpu_installation_input)
-    # NVIDIA proprietary graphic card driver installation
     if nvidia == 'n':
         print(nvidiagpu_skip)
     elif nvidia == 'y' or 'Y':
@@ -22,6 +21,7 @@ else:
     if corectrl == 'n':
         print(corectrl_skip)
     elif corectrl == 'Y' or 'y':
-        os.system("sudo dnf install -y corectrl")
+        print("Installing CoreCtrl ...")
+        os.system("sudo dnf install -y corectrl > /dev/null 2>&1")
         print('\033[92m' + corectrl_status)
 #print('\033[1m' + 'post-configuration of Fedora was successfull!' + '\033[0m')
