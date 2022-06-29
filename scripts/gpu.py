87b850b97154bca5d72128c9422b8dc1ac9c015f
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-06-29 11:10:39 +0200

    Update gpu.py

--- scripts/gpu.py
+++ scripts/gpu.py
@@ -12,7 +12,7 @@ if os.path.exists("/dev/nvidia0"):
         print("→ installing proprietary nVidia Linux akmod graphic card driver:")
         os.system("sudo dnf install -y akmod-nvidia")
         print('\033[1m' + '→→ OK' + '\033[0m')
-        print('\033[93m' + 'For your nVidia GPU settings to take effect, you need to restart your computer' + '\033[93m')
+        print('\033[93m' + 'For your nVidia GPU settings to take effect, you need to restart your computer' + '\033[92m')
 else:
     print("→ installation of program CoreCtrl for simple settings GPUs by AMD")
     corectrl = input("→→ so do you wish install corectrl? [Y/n]: ")
