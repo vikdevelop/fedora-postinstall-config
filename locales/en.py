dda1a497bbecd8385fdeefcd086066f9ecd7cb7f
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-06-29 17:56:37 +0200

    Update English translations

--- locales/en.py
+++ locales/en.py
@@ -5,7 +5,8 @@
 checking = '→ checking if it is a Fedora distribution '
 xerror = 'it looks like the Fedora distribution is not installed on this HW, or it is corrupted. In this case, you cannot use this script :('
 sudo = '→→ we will now ask you for your password within sudo that choosed in installation of Fedora, because we need to update the Fedora distribution packages to the latest version:'
-update_status = '→ updatation Fedory '
+update_will_take_time = "If updates are available, it will take a while. Don't worry though, it will only take a few minutes. When the updates are complete, you will see 'updatation Fedora OK' below, so you can continue."
+update_status = '→ updatation Fedora '
 
 # Summary
 summary_title = "Summary: "
@@ -16,6 +17,8 @@ canceled = "Canceled."
 # GRUB-BTRFS configuration
 grub_btrfs_title = "→ configuration GRUB + Btrfs"
 timeshift_make = "→→ checking if is installed timehift and make"
+installing_timeshift = "→→→ installing timeshift ..."
+installing_make = "→→→ installing make ..."
 grub_btrfs_downloading = "→→ downloading repository grub-brtfs"
 download_folder = "Downloads"
 make_status = "→→→ compilation & installation with program make was successfull"
@@ -28,8 +31,11 @@ flatpak_installation_title = "→ installation recommended Flatpak apps"
 flatpak_installation_desc = "If you want, you can skip this step by pressing the enter key (press the c key to continue): "
 skip_flatpakinstall = "installation Flatpak apps: I skiping."
 flatseal = "Do you want install the Flatseal app that allows you to set permissions on Flatpak apps? [Y/n]: "
+installing_flatseal = "Installing Flatseal ..."
 em = "Do you want to install the Extension Manager application that allows you to manage extensions for GNOME (Fedora Workstation edition)? [Y/n]: "
+installing_em = "Installing Extension Manager ..."
 dw = "Do you want to install Dynamic Wallpaper, an application that allows you to set transient wallpapers in GNOME? [Y/n]: "
+installing_dw = "Installing Dynamic Wallpaper ..."
 flatseal_installed_status = "Flatseal already installed."
 em_installed_status = "Extension Manager already installed."
 dw_installed_status = "Dynamic Wallpaper already installed."
