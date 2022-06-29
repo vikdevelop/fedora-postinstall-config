d0d54a01872b3e6564b952db2dbd643efde89cd0
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-06-29 14:24:32 +0200

    Update fedora_postinstall_config.py

--- fedora_postinstall_config.py
+++ fedora_postinstall_config.py
@@ -5,8 +5,6 @@ import subprocess
 if subprocess.getoutput("locale") == 'LANG=cs_CZ.UTF-8\nLC_CTYPE="cs_CZ.UTF-8"\nLC_NUMERIC="cs_CZ.UTF-8"\nLC_TIME="cs_CZ.UTF-8"\nLC_COLLATE="cs_CZ.UTF-8"\nLC_MONETARY="cs_CZ.UTF-8"\nLC_MESSAGES="cs_CZ.UTF-8"\nLC_PAPER="cs_CZ.UTF-8"\nLC_NAME="cs_CZ.UTF-8"\nLC_ADDRESS="cs_CZ.UTF-8"\nLC_TELEPHONE="cs_CZ.UTF-8"\nLC_MEASUREMENT="cs_CZ.UTF-8"\nLC_IDENTIFICATION="cs_CZ.UTF-8"\nLC_ALL=':
     from locales.cs import *
     import scripts.main
-    import scripts.gpu
 else:
     from locales.en import *
     import scripts.main
-    import scripts.gpu
