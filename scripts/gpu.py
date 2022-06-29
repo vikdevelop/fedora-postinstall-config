778fd61fce67d0925131f9f4564f11f942d05ae4
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-06-29 17:24:16 +0200

    Update gpu.py

--- scripts/gpu.py
+++ scripts/gpu.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import os
 import sys
-sys.path.append("/tmp/fedora-postinstall-config/locales")
-from en import *
+sys.path.append("/tmp/fedora-postinstall-config/scripts")
+from main import *
 if os.path.exists("/dev/nvidia0"):
     print(nvidiagpu_installation_title)
     nvidia = input(nvidiagpu_installation_input)
